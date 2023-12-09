7dc6f154016cbbde812ff3563be0e39aec5bc951
Steven <schoons@live.com>
2023-12-09 12:47:45 +0800

    Second commit

--- Program.cs
+++ Program.cs
@@ -5,21 +5,36 @@ namespace Test {
     class Program {
         public static void Main(string[] args)
         {
-            Point x;
-            x.X = 10;
-            x.Y = 11;
-            Console.WriteLine(x.ToString());
+
+            //Point x = new Point(10,11);
+            //Console.WriteLine(x.ToString());
+
+            Point y = new Point();
+            Console.WriteLine(y.ToString());
 
         }
 
     }
 
-    struct Point {
-        public int X;
-        public int Y;
+    readonly struct Point {
+        //public readonly int X;
+        
+        //public readonly int Y;
+        public int X {get;}
+        public int Y {get;}
 
         public override string ToString(){
             return string.Format("X = {0}, Y = {1}", X,Y);
+        }
+
+        public Point() {
+            X = 1;
+            Y = 1;
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
         }
     }
 
