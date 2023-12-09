44f6959b53b916f64b9008b86bb1236c196010d7
Steven <schoons@live.com>
2023-12-09 10:50:11 +0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test {
+
+    class Program {
+        public static void Main(string[] args)
+        {
+            Point x;
+            x.X = 10;
+            x.Y = 11;
+            Console.WriteLine(x.ToString());
+
+        }
+
+    }
+
+    struct Point {
+        public int X;
+        public int Y;
+
+        public override string ToString(){
+            return string.Format("X = {0}, Y = {1}", X,Y);
+        }
+    }
+
+}
